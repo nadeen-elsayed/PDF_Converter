2473b11765c3a17dc8a2a8edee0bc8817908634f
nadeen-elsayed <dodynadeen0@gmail.com>
2024-08-16 20:39:51 +0300

    actual final update

--- pdf_converter/settings.py
+++ pdf_converter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3#wubp%#qt*n5akocx4+^$7=j9hpe@hel&!(#)nr-@qzkwsv_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 import dj_database_url
 
