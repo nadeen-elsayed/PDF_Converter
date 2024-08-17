08e33c999052d75368445941ae9d6dd8c8a19ff4
nadeen-elsayed <dodynadeen0@gmail.com>
2024-08-17 18:28:19 +0300

    fix errors

--- pdf_converter/settings.py
+++ pdf_converter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '3#wubp%#qt*n5akocx4+^$7=j9hpe@hel&!(#)nr-@qzkwsv_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
